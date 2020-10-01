958e5f5794b81678784be8bc4aa4df27a4201d08
Javier de Arcos <javier.dearcos@liferay.com>
2020-10-01 10:25:46 +0200

    Generate code with REST Builder

---
+++ modules/liferay-travel-rest/liferay-travel-rest-api/src/main/java/liferay/travel/rest/resource/v1_0/StageResource.java
@@ -0,0 +1,106 @@
+package liferay.travel.rest.resource.v1_0;
+
+import com.liferay.portal.vulcan.accept.language.AcceptLanguage;
+import com.liferay.portal.vulcan.pagination.Page;
+
+import java.util.Locale;
+
+import javax.annotation.Generated;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import liferay.travel.rest.dto.v1_0.Stage;
+
+import org.osgi.annotation.versioning.ProviderType;
+
+/**
+ * To access this resource, run:
+ *
+ *     curl -u your@email.com:yourpassword -D - http://localhost:8080/o/liferay-travel-rest/v1.0
+ *
+ * @author Javier de Arcos
+ * @generated
+ */
+@Generated("")
+@ProviderType
+public interface StageResource {
+
+	public static Builder builder() {
+		return FactoryHolder.factory.create();
+	}
+
+	public Page<Stage> getTripStagesPage(Long tripId) throws Exception;
+
+	public Stage postTripStage(Long tripId, Stage stage) throws Exception;
+
+	public Response postTripStageBatch(
+			Long tripId, String callbackURL, Object object)
+		throws Exception;
+
+	public void deleteStage(Long stageId) throws Exception;
+
+	public Response deleteStageBatch(String callbackURL, Object object)
+		throws Exception;
+
+	public Stage getStage(Long stageId) throws Exception;
+
+	public Stage putStage(Long stageId, Stage stage) throws Exception;
+
+	public Response putStageBatch(String callbackURL, Object object)
+		throws Exception;
+
+	public default void setContextAcceptLanguage(
+		AcceptLanguage contextAcceptLanguage) {
+	}
+
+	public void setContextCompany(
+		com.liferay.portal.kernel.model.Company contextCompany);
+
+	public default void setContextHttpServletRequest(
+		HttpServletRequest contextHttpServletRequest) {
+	}
+
+	public default void setContextHttpServletResponse(
+		HttpServletResponse contextHttpServletResponse) {
+	}
+
+	public default void setContextUriInfo(UriInfo contextUriInfo) {
+	}
+
+	public void setContextUser(
+		com.liferay.portal.kernel.model.User contextUser);
+
+	public static class FactoryHolder {
+
+		public static volatile Factory factory;
+
+	}
+
+	@ProviderType
+	public interface Builder {
+
+		public StageResource build();
+
+		public Builder checkPermissions(boolean checkPermissions);
+
+		public Builder httpServletRequest(
+			HttpServletRequest httpServletRequest);
+
+		public Builder preferredLocale(Locale preferredLocale);
+
+		public Builder user(com.liferay.portal.kernel.model.User user);
+
+	}
+
+	@ProviderType
+	public interface Factory {
+
+		public Builder create();
+
+	}
+
+}
---
+++ modules/liferay-travel-rest/liferay-travel-rest-impl/src/main/java/liferay/travel/rest/internal/graphql/servlet/v1_0/ServletDataImpl.java
@@ -0,0 +1,63 @@
+package liferay.travel.rest.internal.graphql.servlet.v1_0;
+
+import com.liferay.portal.vulcan.graphql.servlet.ServletData;
+
+import javax.annotation.Generated;
+
+import liferay.travel.rest.internal.graphql.mutation.v1_0.Mutation;
+import liferay.travel.rest.internal.graphql.query.v1_0.Query;
+import liferay.travel.rest.resource.v1_0.StageResource;
+import liferay.travel.rest.resource.v1_0.TripResource;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.ComponentServiceObjects;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceScope;
+
+/**
+ * @author Javier de Arcos
+ * @generated
+ */
+@Component(immediate = true, service = ServletData.class)
+@Generated("")
+public class ServletDataImpl implements ServletData {
+
+	@Activate
+	public void activate(BundleContext bundleContext) {
+		Mutation.setStageResourceComponentServiceObjects(
+			_stageResourceComponentServiceObjects);
+		Mutation.setTripResourceComponentServiceObjects(
+			_tripResourceComponentServiceObjects);
+
+		Query.setStageResourceComponentServiceObjects(
+			_stageResourceComponentServiceObjects);
+		Query.setTripResourceComponentServiceObjects(
+			_tripResourceComponentServiceObjects);
+	}
+
+	@Override
+	public Mutation getMutation() {
+		return new Mutation();
+	}
+
+	@Override
+	public String getPath() {
+		return "/liferay-travel-rest-graphql/v1_0";
+	}
+
+	@Override
+	public Query getQuery() {
+		return new Query();
+	}
+
+	@Reference(scope = ReferenceScope.PROTOTYPE_REQUIRED)
+	private ComponentServiceObjects<StageResource>
+		_stageResourceComponentServiceObjects;
+
+	@Reference(scope = ReferenceScope.PROTOTYPE_REQUIRED)
+	private ComponentServiceObjects<TripResource>
+		_tripResourceComponentServiceObjects;
+
+}
